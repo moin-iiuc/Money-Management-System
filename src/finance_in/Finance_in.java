bd426544daec2ddce33d69e69959215cca8fdfcf
moin-iiuc <mohammadmhbhuiyan@gmail.com>
2024-01-13 00:17:08 +0600

    my first

---
+++ src/finance_in/Finance_in.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finance_in;
+
+/**
+ *
+ * @author DELL
+ */
+public class Finance_in {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
